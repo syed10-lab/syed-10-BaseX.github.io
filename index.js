1c4731aadb2b85f71e2ec7b09a0c0c1d7688ed5a
freelink <tradewithsyed63@gmail.com>
2025-12-14 11:59:06 +0530

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const authRoutes = require("./auth");
-
-const app = express();
-app.use(express.json());
-
-authRoutes(app);
-
-app.listen(3000, () => console.log("BaseX Auth running"));
