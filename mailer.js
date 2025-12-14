f038bad78af29c3d1b1c11b58927de5ea98c9622
freelink <tradewithsyed63@gmail.com>
2025-12-14 13:55:29 +0530

    Create mailer.js

---
+++ mailer.js
@@ -0,0 +1,28 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS
+  }
+});
+
+export async function sendOTP(email, otp) {
+  console.log("📧 Sending OTP to:", email);
+
+  const info = await transporter.sendMail({
+    from: `"BaseX" <${process.env.MAIL_USER}>`,
+    to: email,
+    subject: "BaseX OTP",
+    html: `
+      <h2>Your BaseX OTP</h2>
+      <p><b>${otp}</b></p>
+      <p>Valid for 10 minutes</p>
+    `
+  });
+
+  console.log("✅ Mail sent:", info.messageId);
+}
